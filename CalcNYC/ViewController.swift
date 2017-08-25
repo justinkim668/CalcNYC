ff0cd19793b3750367bad73673ede0e403af21e9
justinkim668 <justinkim668@gmail.com>
2017-08-24 23:23:11 -0400

    Initial Commit

---
+++ CalcNYC/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CalcNYC
+//
+//  Created by Justin Kim on 8/24/17.
+//  Copyright © 2017 Justin Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
